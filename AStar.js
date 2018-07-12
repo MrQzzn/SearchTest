ccb7028eade03989b8bef4a4aa436aadaa9bd023
Daniel Cheng <danielcheng0105@gmail.com>
2018-07-11 23:28:32 -0400

    Update AStar.js

--- AStar.js
+++ AStar.js
@@ -1,6 +1,5 @@
 create();
 
-var graph = [];
 //var path = []; //for the actual path, not closed list
 var rows;
 var cols;
@@ -15,28 +14,44 @@ function input(){
     weight = parseInt(document.getElementById("weight").value);
     document.write("START = " + start +  " END = " + end + " WEIGHT = " + weight);
 }
+function initGrid(){
 
-function drawGrid(closed, dist) {
+}
+function drawGrid(closed, dist, graph) {
     var c = document.getElementById('canvas');
     var ctx = c.getContext('2d');
     ctx.strokeStyle = 'black';
     ctx.lineWidth = 4;
 
     //mClosed.sort( function(a, b) { return a - b; } );
-
+    for (var i = 0; i < 10; i++){
+        for (var j = 0; j < 10; j++){
+            document.write(graph[i][j] + " ");
+        }
+        document.write("<br>");
+    }
 
     var i = 0;
-    for (var y = 0; y < 5; y++) {
-        for (var x = 0; x < 5; x++) { //add boxes by rows
-            document.write("i = " + i + " " + closed[i] + "<br>");
-            if ((closed.length > i) && (closed[i] === true)) {
+    for (var y = 0; y < cols; y++) {
+        for (var x = 0; x < rows; x++) { //add boxes by rows
+            if (i === source){
+                ctx.fillStyle = 'rgba(196,0,255,1)'; //source = purple
+            }
+            else if (i === end){
+                ctx.fillStyle = 'rgba(256,0,0,1)'; //goal = red
+            }
+            else if (graph[Math.floor(i/rows)][i%cols] === 0){ //a wall
+                document.write("index " + i + " is a wall");
+                ctx.fillStyle = 'rgba(165,165,165,1)';
+            }
+            else if (closed[i] === true) { //visited
                 ctx.fillStyle = 'rgba(0,256,0,1)';
                 //i++; i++ in if statement only for numerical array of visited indices
             }
-            else if (dist[y*rows + x] !== -1)
+            else if (dist[y*rows + x] !== -1) //not visited but are accounted for, "adjacent boxes"
                 ctx.fillStyle = 'rgba(0,0,256,1)';
-            else
-                ctx.fillStyle = 'rgba(0,0,0,1)';
+            else                              //everything not visited or adjacent
+                ctx.fillStyle = 'rgba(256,256,256,1)';
             i++;
             ctx.beginPath();
             ctx.moveTo(x * 100, y * 100);
@@ -54,32 +69,46 @@ function drawGrid(closed, dist) {
 }
 
 function create(){
+    var graph = [];
     //input();
     counter = 0;
-    rows = 5;
-    cols = 5;
-    graph = [[1, 2, 0, 3, 4],
-            [3, 3, 3, 0, 3],
-            [2, 0, 4, 7, 3],
-            [3, 2, 3, 0, 3],
-            [5, 4, 7, 4, 1]];
-    weight = 0;
+    rows = 10;
+    cols = 10;
+    createGraph(graph);
+    weight = 20;
 
     source = 0;
-    end = 24;
-    search();
+    end = 99;
+    search(graph);
+}
+function createGraph(graph){
+
+    document.write("here");
+    for (var i = 0; i < rows; i++){
+        graph[i] = [];
+        for (var j = 0; j < cols; j++){
+            graph[i][j] = Math.floor(Math.random() * 10);
+        }
+    }
+    for (var i = 0; i < 10; i++){
+        for (var j = 0; j < 10; j++){
+            document.write(graph[i][j] + " ");
+        }
+        document.write("<br>");
+    }
+    return graph;
 }
 function heuristic(E){
     document.write("heuristic values: ");
     for (var i = 0; i < rows*cols; i++){
         E.push(Math.abs(Math.floor(end/rows) - Math.floor(i/rows)) + Math.abs(end%rows - i%rows))
-        if (counter%5 === 0)
+        if (counter%rows === 0)
             document.write("<br>");
         document.write(E[i] + " ");
         counter++;
     }
 }
-function search() {
+function search(graph) {
     var closed = [];
     var mClosed = [];
     var dist = [];
@@ -96,14 +125,14 @@ function search() {
     }//initialize arrays
     document.write("<br> initial dist[]:");
     for (var i = 0; i < rows*cols; i++){
-        if (counter%5 === 0)
+        if (counter%rows === 0)
             document.write("<br>");
         document.write(dist[i] + " ");
         counter++;
     }
     document.write("<br> initial closed[]:")
     for (var i = 0; i < rows*cols; i++){
-        if (counter%5 === 0)
+        if (counter%rows === 0)
             document.write("<br>");
         document.write(closed[i] + " ");
         counter++;
@@ -119,7 +148,7 @@ function search() {
 
             if (!closed[adjIndex]
                 && graph[Math.floor(adjIndex / rows)][adjIndex % cols] !== 0
-                && isAdjacent(dist, closed, adjIndex)
+                && isAdjacent(closed, adjIndex)
                 && adjIndex !== lowestAdj
                 && dist[adjIndex] < dist[lowestAdj] + graph[Math.floor(adjIndex / rows)][adjIndex % cols]
                 && dist[adjIndex] === -1) {
@@ -127,7 +156,7 @@ function search() {
                 dist[adjIndex] = dist[lowestAdj] + graph[Math.floor(adjIndex / rows)][adjIndex % cols];
 
                 for (var k = 0; k < rows*cols; k++) {
-                    if (counter%5 === 0)
+                    if (counter%rows === 0)
                         document.write("<br>");
                     document.write(dist[k]+ " ");
                     counter++;
@@ -144,7 +173,7 @@ function search() {
 
     }
     printPath(dist);
-    drawGrid(closed, dist);
+    drawGrid(closed, dist, graph);
 }
 function findMin(dist, E, closed, graph){
 
@@ -166,11 +195,11 @@ function findMin(dist, E, closed, graph){
     }
     return minIndex;
 }
-function isAdjacent(dist, closed, index){
+function isAdjacent(closed, index){
 
     if (Math.floor(index/rows) !== 0) {
 
-        if (closed[index-5] === true) {
+        if (closed[index-rows] === true) {
 
             return true;
         }
@@ -184,7 +213,7 @@ function isAdjacent(dist, closed, index){
         }
     }
     //is right bounded, can check right, exception made for (4, 4)
-    if (index !== 24 && index%rows !== 4) {
+    if (index !== 99 && index%rows !== 9) {
 
         if (closed[index+1] === true) {
 
@@ -192,9 +221,9 @@ function isAdjacent(dist, closed, index){
         }
     }
     //is lower bounded, can check down
-    if (Math.floor(index/rows) !== 4) {
+    if (Math.floor(index/rows) !== 9) {
 
-        if (closed[index+5] === true) {
+        if (closed[index+rows] === true) {
 
             return true;
         }
